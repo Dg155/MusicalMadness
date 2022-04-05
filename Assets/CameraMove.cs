a02fd066d9ddbb2e8590385cd6be9a70fb693b93
hungbuiwork <hungbuiwork@gmail.com>
2022-04-05 15:11:07 -0700

    Tilemaps

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Vector3 pos;
+    public bool moving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
