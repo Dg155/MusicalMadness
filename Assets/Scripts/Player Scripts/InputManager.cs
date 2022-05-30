91f62fb4d03f5dd997c3a79d7349935dfdc1b328
Ryan Bascos <57274168+Involuta@users.noreply.github.com>
2022-05-29 22:24:51 -0700

    Buffer System
    omg an actual buffer system

--- Assets/Scripts/Player Scripts/InputManager.cs
+++ Assets/Scripts/Player Scripts/InputManager.cs
@@ -8,6 +8,10 @@ public class InputManager : MonoBehaviour
     PlayerMove playerMove;
     Combat playerCombat;
     Animator animator;
+    bool primaryBuffered;
+    bool secondaryBuffered;
+    public float bufferTime;
+
     void Start()
     {
         animator = this.GetComponent<Animator>();
@@ -26,12 +30,14 @@ public class InputManager : MonoBehaviour
             animator.SetFloat("Speed", 0);
         }
         if (Input.GetMouseButton(0)){
-            UseMainHand(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            StartCoroutine(BufferPrimary());
         }
         if (Input.GetMouseButton(1)){
             //UseOffHand(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            UseMainHandSecondary(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            StartCoroutine(BufferSecondary());
         }
+        UseMainHand(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        UseMainHandSecondary(Camera.main.ScreenToWorldPoint(Input.mousePosition));
     }
 
     void FixedUpdate()
@@ -39,12 +45,40 @@ public class InputManager : MonoBehaviour
         playerMove.Move(horizontal, vertical);
     }
 
+    private IEnumerator BufferPrimary()
+    {
+        Debug.Log("primary buffered!");
+        if (!primaryBuffered)
+        {
+            primaryBuffered = true;
+            yield return new WaitForSeconds(bufferTime);
+            primaryBuffered = false;
+        }
+        Debug.Log("primary no longer buffered");
+    }
+
+    private IEnumerator BufferSecondary()
+    {
+        Debug.Log("secondary buffered!");
+        if (!secondaryBuffered)
+        {
+            secondaryBuffered = true;
+            yield return new WaitForSeconds(bufferTime);
+            secondaryBuffered = false;
+        }
+        Debug.Log("secondary no longer buffered");
+    }
+
     private void UseMainHand(Vector3 mousePos) {
-        playerCombat.UseMainHand(mousePos);
+        if (primaryBuffered){
+            playerCombat.UseMainHand(mousePos);
+        }
     }
 
     private void UseMainHandSecondary(Vector3 mousePos) {
-        playerCombat.UseMainHandSecondary(mousePos);
+        if (secondaryBuffered){
+            playerCombat.UseMainHandSecondary(mousePos);
+        }
     }
 
     private void UseOffHand(Vector3 mousePos) {
