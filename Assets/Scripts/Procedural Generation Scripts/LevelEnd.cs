359a5d7006dfc9df2a59ac3dc7384ec5133a8aa8
hungbuiwork <hungbuiwork@gmail.com>
2022-06-03 17:17:37 -0700

    fixed hehe

--- Assets/Scripts/Procedural Generation Scripts/LevelEnd.cs
+++ Assets/Scripts/Procedural Generation Scripts/LevelEnd.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class LevelEnd : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag != "Player"){
+            return;
+        }
         FindObjectOfType<LevelLoader>().LoadNextScene();
     }
 }
